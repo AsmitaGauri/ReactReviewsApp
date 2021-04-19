d6fae0d7ba6bfa167b64cd691349ef94514d985b
Asmita Gauri <asmitagaurikhan@gmail.com>
2021-04-19 20:23:44 +0530

    Done with the small project

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -40,6 +40,7 @@ export default function ReviewForm({addReview}) {
 
                         <TextInput
                          style={globalStyles.input}
+                         multiline minHeight={60}
                          placeholder="Review body"
                          onChangeText={formikProps.handleChange('body')}
                          value={formikProps.values.body}
